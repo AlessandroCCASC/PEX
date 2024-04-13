397384cf6fa49a9a0ad81652b568edcec5f40eda
Lucas Sabino <lucas_sabinomf@hotmail.com>
2024-04-13 14:30:28 -0300

    ajuste no quem somos

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,7 +5,7 @@ export function Info() {
 	return (
 		<section id="who-we-are">
 			<div className="flex flex-grow items-center justify-center overflow-hidden py-16 px-8 md:px-16 bg-accent-50">
-				<div className='md:flex flex-grow max-w-5xl'>
+				<div className='md:flex flex-grow max-w-5xl items-center justify-center'>
 					<div className='p-8'>
 						<p className='block mt-1 md:text-lg font-semibold leading-tight text-gray-900 text-justify'>Quem somos</p>
 						<p className='block mt-1 md:text-md leading-tight text-gray-700 text-justify'>Com uma história enraizada na paixão pela beleza, Taty Unhas e Beleza é o destino definitivo para cuidados de unhas e beleza. Nosso compromisso é realçar sua beleza natural enquanto cultivamos uma atmosfera acolhedora de confiança e elegância. Junte-se a nós para uma experiência única de estilo e autoestima.</p>
